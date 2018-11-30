b3937b9d43406cc291399631ecff9e95e05145e1
Carter Tazio Schonwald <carter.schonwald@gmail.com>
2018-11-30 16:35:12 -0500

    dirty state for sharing

--- src/Crypto/SecureHash/SHA3.hs
+++ src/Crypto/SecureHash/SHA3.hs
@@ -1,2 +1,8 @@
 
-module Crypto.SecureHash.SHA3 where
+module Crypto.SecureHash.SHA3(sha3_512) where
+
+import Crypto.SecureHash.SHA3.FFI
+import qualified Data.Bytestring as BS
+import Data.Word (Word32)
+import Foreign.C.String (CStringLen)
+
--- src/Crypto/SecureHash/SHA3/FFI.hs
+++ src/Crypto/SecureHash/SHA3/FFI.hs
@@ -1,2 +1,28 @@
 
 module Crypto.SecureHash.SHA3.FFI where
+
+import Foreign.C.Types
+import Foreign.Ptr
+import Data.Word
+
+{-
+
+/* *
+  *  Function to compute SHA3-512 on the input message. The output length is fixed to 64 bytes.
+  */
+void CTS_FIPS202_SHA3_512(const unsigned char *input, unsigned int inputByteLen, unsigned char *output)
+{
+    CTS_Keccak(576, 1024, input, inputByteLen, 0x06, output, 64);
+}
+
+
+-}
+
+
+{- NOTE: CUChar or Word
+
+-}
+foreign import ccall unsafe "CTS_FIPS202_SHA3_512"
+  c_unsafe_FIPS202_SHA3_512 :: Ptr CChar -> CUInt -> Ptr Word8 -> IO ()
+foreign import ccall safe "CTS_FIPS202_SHA3_512"
+  c_safe_FIPS202_SHA3_512 :: Ptr CChar -> CUInt -> Ptr Word8 -> IO ()
